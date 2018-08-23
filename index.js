90832f4bc277d4e4a34bebbf6fd107398291cb62
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-23 12:13:45 -0400

    Throw TypeErrors during command registry

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ const rqAll = require("require-all");
 
 function register(cmdObj) {
 	const cmd = Object.assign({}, cmdObj);
-
-	cmdRegistry[cmd.name] = cmd;
-	cmd.aliases.forEach(alias => cmdRegistry[alias] = cmd);
+	
+	if (!cmd.name) {
+		const err = new TypeError("A command must have a name.");
+		err.code = "COMMAND_MISSING_NAME";
+		
+		throw err;
+	} else if (!typeof cmd === "object") {
+		const err = new TypeError("A command must be specified as an object.");
+		err.code = "COMMAND_NOT_OBJECT";
+		
+		throw err;
+	} else {
+		cmdRegistry[cmd.name] = cmd;
+		cmd.aliases.forEach(alias => cmdRegistry[alias] = cmd);
+	}
 }
 
 function registerDirectory(directory = "", recursive = true) {
