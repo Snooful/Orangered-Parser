6bf5574dc7e288f9132733a8fda3e04d99e8a524
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-22 22:49:34 -0500

    Move Command class into its own file

--- src/index.js
+++ src/index.js
@@ -24,63 +24,7 @@ module.exports.InvalidArgumentError = InvalidArgumentError;
 const CommandError = require("./errors/command.js");
 module.exports.CommandError = CommandError;
 
-class Command {
-	constructor(command) {
-		if (command.name.includes(" ")) {
-			const error = new Error("Command names may not include spaces.");
-			error.code = "SPACE_IN_COMMAND_NAME";
-			throw error;
-		}
-		this.name = command.name;
-
-		this.originalName = command.originalName || command.name;
-
-		this.description = command.description;
-		this.longDescription = command.longDescription;
-
-		if (command.arguments) {
-			this.arguments = command.arguments.map(arg => {
-				if (arg instanceof argTypes.generic) {
-					return arg;
-				} else if (argTypes[arg.type]) {
-					return new argTypes[arg.type](arg);
-				} else {
-					return new argTypes.string(arg);
-				}
-			});
-		} else {
-			this.arguments = [];
-		}
-
-		this.aliases = command.aliases || [];
-
-		this.check = command.check;
-
-		this.handler = command.handler;
-	}
-
-	/**
-		* Generates a string based off of the command and its arguments.
-		* @param {boolean} differentiateRequired If true, uses a separate set of brackets for required arguments.
-		* @returns {string} The command with arguments.
-	*/
-	usage(differentiateRequired = true) {
-		const wrapped = this.arguments.map(arg => {
-			if (arg.required && differentiateRequired) {
-				return `<${arg.key}>`;
-			} else {
-				return `[${arg.key}]`;
-			}
-		});
-		return `${this.name} ${wrapped.join(" ")}`;
-	}
-
-	run(args) {
-		if (this.handler) {
-			this.handler(args);
-		}
-	}
-}
+const Command = require("./command.js");
 module.exports.Command = Command;
 
 /**
