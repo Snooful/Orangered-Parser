e45a2baa20b7f25c1dba64004f646771eeb1a4a6
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-23 11:28:41 -0500

    Rename min and max properties of string argument

--- src/arguments/string.js
+++ src/arguments/string.js
@@ -7,8 +7,8 @@ class StringArgument extends Argument {
 
 		this.type = "string";
 
-		this.min = argument.minLength || 0;
-		this.max = argument.maxLength || Infinity;
+		this.minLength = argument.minLength || 0;
+		this.maxLength = argument.maxLength || Infinity;
 
 		this.matches = new RegExp(argument.matches);
 	}
@@ -21,9 +21,9 @@ class StringArgument extends Argument {
 		const str = value.toString();
 		if (!this.matches.test(str)) {
 			return new InvalidArgumentError(this.constructor, args, "string_argument_regexp_fail", this, value);
-		} else if (str.length >= this.max) {
+		} else if (str.length >= this.maxLength) {
 			return new InvalidArgumentError(this.constructor, args, "string_argument_too_long", this, value);
-		} else if (str.length <= this.min) {
+		} else if (str.length <= this.minLength) {
 			return new InvalidArgumentError(this.constructor, args, "string_argument_too_short", this, value);
 		} else {
 			return str;
